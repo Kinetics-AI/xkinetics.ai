72da06875171c3c7d311a4ae8fd1f4b7d2a50422
faikit <huijie.wang@hotmail.com>
2025-08-25 00:47:41 +0800

    .

--- app/[locale]/(default)/(home)/page.tsx
+++ app/[locale]/(default)/(home)/page.tsx
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 
-export default function Home({ params }: { params: { locale: string } }) {
+export default function Home({ params }: { params: Promise<{ cat: string }> }) {
 
     const { locale } = params;
     
