1df2842cd183c5ccd4dde4b04b7d7f90b7e4a91d
faikit <huijie.wang@hotmail.com>
2025-08-25 01:00:05 +0800

    .g

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -56,10 +56,12 @@ export function generateStaticParams() {
     return routing.locales.map((locale) => ({locale}));
 }
 
-export default async function LocaleLayout({
-    children,
-    params
-}: LayoutProps<'/[locale]'>) {
+interface LocaleLayoutProps {
+  children: React.ReactNode;
+  params: { locale: string };
+}
+
+export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
     const {locale} = params;
     if (!hasLocale(routing.locales, locale)) {
         notFound();
